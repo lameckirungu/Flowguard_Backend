9991c9930ff52689442515bfbc95909a33c63c9a
Lameck Irungu <mugolameck@gmail.com>
2026-09-08 08:59:40 +0300

    fix: normalize reconciled migration lineage

--- migrations/versions/c0f8a6d7e210_merge_reconciled_heads.py
+++ migrations/versions/c0f8a6d7e210_merge_reconciled_heads.py
@@ -1,7 +1,7 @@
 """Merge the upstream governance graph with the fork operational graph."""
 
 revision = "c0f8a6d7e210"
-down_revision = ("a107c6206701", "a1b2c3d4e5f6")
+down_revision = "a1b2c3d4e5f6"
 branch_labels = None
 depends_on = None
 
